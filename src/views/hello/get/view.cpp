7165e152399b535ef6d3c7ffe325d4eca65f4467
tiescl <anvar.madvaliev@icloud.com>
2025-11-15 14:07:05 +0500

    cc: improve pswd hashing logic

--- src/views/hello/get/view.cpp
+++ src/views/hello/get/view.cpp
@@ -16,19 +16,32 @@
 
 namespace handlers::hello::get {
 
+static constexpr int kSaltLength{16};
+static constexpr int kHashLength{32};
+static constexpr int kIterations{100000};
+
 Handler::Handler(const components::ComponentConfig& config, const components::ComponentContext& context)
     : HttpHandlerBase(config, context), pg_(context) {}
 
 std::string Handler::HandleRequest(server::http::HttpRequest& request, server::request::RequestContext&) const {
-    unsigned char salt[16];
-    RAND_bytes(salt, 16);
+    unsigned char salt[kSaltLength];
+    RAND_bytes(salt, kSaltLength);
 
-    unsigned char hash[32];
+    unsigned char hash[kHashLength];
     const auto& name = request.GetArg("name");
-    PKCS5_PBKDF2_HMAC(name.c_str(), name.length(), salt, 16, 10000, EVP_sha256(), 32, hash);
+    PKCS5_PBKDF2_HMAC(name.c_str(), name.length(), salt, kSaltLength, kIterations, EVP_sha256(), kHashLength, hash);
 
-    std::string hash_str(reinterpret_cast<const char*>(hash), 16);
-    LOG_INFO("tiescl(hash): {}", hash_str);
+    std::string result;
+    result.reserve(kSaltLength + 1 + kHashLength);
+    for (int i = 0; i < kSaltLength; i++) {
+        result += std::format("{:02x}", salt[i]);
+    }
+    result += "$";
+    for (int i = 0; i < kHashLength; i++) {
+        result += std::format("{:02x}", hash[i]);
+    }
+
+    LOG_INFO("tiescl(pswd_hash): {}", result);
 
     return utils::SayHelloTo(name, utils::UserType::kFirstTime, db::api::SelectNow(pg_));
 }
